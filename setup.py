cd6a7fe21790036a8606e398dfe4b145c1b3fbc6
nakker <nakker@gmail.com>
2023-04-26 11:16:06 -0600

    Update setup.py
    Allow python 3.8

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author='Robin Winter',
     author_email='robin.winter@bayer.com',
     description='continous and data-driven molecular descriptors (CDDD)',
-    python_requires='>=3.6.1, <3.7',
+    python_requires='>=3.6.1, <3.8',
     install_requires=[
         'tensorflow-gpu==1.10.0',
         'scikit-learn',
